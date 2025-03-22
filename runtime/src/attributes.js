f0847596978146f6468ba6606e6d4a829ae48733
karl <enriquezkharl14@gmail.com>
2025-03-23 00:55:12 +0800

    [Feat] Adding of attributes

--- runtime/src/attributes.js
+++ runtime/src/attributes.js
@@ -16,5 +16,41 @@ export function setAttributes(el, attrs) {
   }
 }
 // TODO: implement setClass
+
+function setClass(el, className) {
+  el.className = '';
+
+  if (typeof className === 'string') {
+    el.className = className;
+  }
+
+  if (Array.isArray(className)) {
+    el.classList.add(...className);
+  }
+}
+
 // TODO: implement setStyle
+function setStyle(el, name, value) {
+  el.style[name] = value;
+}
+
+function removeStyle(el, name) {
+  el.style[name] = null;
+}
+
 // TODO: implement setAttribute
+
+function setAtrribute(el, name, value) {
+  if (value === null) {
+    removeAtrribute(el, name);
+  } else if (name.startsWit('data-')) {
+    el.setAtrribute(name, value);
+  } else {
+    el[name] = value;
+  }
+}
+
+function removeAtrribute(el, name) {
+  el[name] = null;
+  el.removeAtrribute(name);
+}
--- runtime/src/mount-dom.js
+++ runtime/src/mount-dom.js
@@ -4,10 +4,13 @@ import { DOM_TYPES } from './h';
 export function mountDOM(vdom, parentEl) {
   switch (vdom.type) {
     case DOM_TYPES.TEXT:
+      createTextNode(vdom, parentEl);
       break;
     case DOM_TYPES.ELEMENT:
+      createElementNode(vdom, parentEl);
       break;
     case DOM_TYPES.FRAGMENT:
+      createFragmentNodes(vdom, parentEl);
       break;
     default:
       throw new Error(`Can't mount DOM of type ${vdom.type}`);
@@ -27,8 +30,11 @@ function createElementNode(vdom, parentEl) {
   const { tag, props, children } = vdom;
 
   const element = document.createElement(tag);
-
+  addprops(element, props, vdom);
   vdom.el = element;
+
+  children.forEach((child) => mountDOM(child, element));
+  parentEl.append(element);
 }
 
 function addprops(el, props, vdom) {
@@ -42,5 +48,5 @@ function createFragmentNodes(vdom, parentEl) {
 
   vdom.el = parentEl;
 
-  childeren.forEach((child) => mountDOM(child, parentEl));
+  children.forEach((child) => mountDOM(child, parentEl));
 }
